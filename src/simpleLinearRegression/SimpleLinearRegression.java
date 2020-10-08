26c36389b387f8bc9d57acdd9a19998faa6bee7e
Carlos VP <ca.varaperez@gmail.com>
2020-10-08 18:10:59 -0500

    Making a better CLI

--- src/simpleLinearRegression/SimpleLinearRegression.java
+++ src/simpleLinearRegression/SimpleLinearRegression.java
@@ -1,13 +1,47 @@
 package simpleLinearRegression;
 
-public class SimpleLinearRegression {
-    private final double beta_1, beta_0;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class SimpleLinearRegression {
+    private static double beta_1 = 0.0;
+    private static double beta_0 = 0.0;
+    private final static double[] x_data = new double[100];
+    private final static double[] y_data = new double[100];
+
+    public static void main(String[] args) throws IOException {
+        int n = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Please specify the dataset size: ");
+        try {
+            n = Integer.parseInt(br.readLine());
+        } catch (NumberFormatException nfe) {
+            System.err.println("Invalid Format!");
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter x: ");
+            try {
+                x_data[i] = Integer.parseInt(br.readLine());
+            } catch (NumberFormatException nfe) {
+                System.err.println("Invalid Format!");
+            }
+            System.out.print("Enter y: ");
+            try {
+                y_data[i] = Integer.parseInt(br.readLine());
+            } catch (NumberFormatException nfe) {
+                System.err.println("Invalid Format!");
+            }
+        }
+        process(x_data, y_data);
+        System.out.print("\nSimple linear regression equation: " + equation());
+        for (int i = 0; i < n; i++) {
+            System.out.print(String.format("\nx: %.2f | predicted y: %.2f", x_data[i], predict(x_data[i])));
+        }
     }
 
-    public SimpleLinearRegression(double[] x, double[] y) {
+    public static void process(double[] x, double[] y) {
         if (x.length != y.length) {
             throw new IllegalArgumentException("array lengths are not equal");
         }
@@ -22,25 +56,25 @@ public class SimpleLinearRegression {
             sigma_y += y[i];
         }
 
-        beta_1 = (sigma_xy - sigma_x * sigma_y) / (sigma_x2 - sigma_x * sigma_x);
+        beta_1 = (n * sigma_xy - sigma_x * sigma_y) / (n * sigma_x2 - sigma_x * sigma_x);
         beta_0 = (sigma_y - beta_1 * sigma_x) / n;
     }
 
-    public double beta_1() {
+    public static double beta_1() {
         return beta_1;
     }
 
-    public double beta_0() {
+    public static double beta_0() {
         return beta_0;
     }
 
-    public double predict(double x) {
+    public static double predict(double x) {
         return beta_0 * x + beta_1;
     }
 
-    public String toString() {
+    public static String equation() {
         StringBuilder s = new StringBuilder();
-        s.append(String.format("%.2f n + %.2f", beta_0(), beta_1()));
+        s.append(String.format("%.2f + %.2f x", beta_1(), beta_0()));
         return s.toString();
     }
 }
