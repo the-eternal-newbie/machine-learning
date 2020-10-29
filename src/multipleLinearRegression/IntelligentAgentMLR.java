2248cf5dbda4ff44dd56a74d4251933257e09ce8
Carlos VP <ca.varaperez@gmail.com>
2020-10-28 21:56:06 -0600

    Implementing class to agent

--- src/multipleLinearRegression/IntelligentAgentMLR.java
+++ src/multipleLinearRegression/IntelligentAgentMLR.java
@@ -4,32 +4,35 @@ import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
 
 public class IntelligentAgentMLR extends Agent {
+    private static final long serialVersionUID = 1L;
+    private String equation;
+    
     protected void setup() {
         System.out.println("Agent " + getLocalName() + " started.");
 
-        Object[] args = getArguments();
-        if (args != null && args.length > 0) {
-            addBehaviour(new Train());
-            addBehaviour(new Predict());
-            predictX = Float.parseFloat((String) args[0]);
-            System.out.println("x to predict = " + predictX);
-        }
+        addBehaviour(new MLR());
 
     }
 
-    private class Predict extends Behaviour {
+    private class MLR extends Behaviour {
         private static final long serialVersionUID = 1L;
+        private MultipleLinearRegression mlr = new MultipleLinearRegression("startups.csv");
 
         @Override
         public void action() {
+            mlr.train();
+            equation = mlr.equationMLR();
         }
 
         @Override
-        public boolean done() {}
+        public boolean done() {
+            return mlr.isTrained();
+        }
 
         @Override
         public int onEnd() {
             myAgent.doDelete();
+            System.out.println(equation);
             return super.onEnd();
         }
     }
--- src/multipleLinearRegression/MultipleLinearRegression.java
+++ src/multipleLinearRegression/MultipleLinearRegression.java
@@ -2,6 +2,7 @@ package multipleLinearRegression;
 
 public class MultipleLinearRegression {
     private int n;
+    private boolean trained = false;
     private double[] yData = new double[100];
     private double[][] xData = new double[2][100];
     private double sigmaX1, sigmaX2, sigmaX12, sigmaXsqr1, sigmaXsqr2, d, beta_0, beta_1, beta_2 = 0.0;
@@ -14,51 +15,66 @@ public class MultipleLinearRegression {
     }
 
     private void determinant() {
-        for (int i = 0; i < n; i++) {
-            sigmaX1 += xData[0][i];
-            sigmaX2 += xData[1][i];
-            sigmaX12 += xData[0][i] * xData[1][i];
-            sigmaXsqr1 += xData[0][i] * xData[0][i];
-            sigmaXsqr2 += xData[1][i] * xData[1][i];
+        if(!trained) {
+            for (int i = 0; i < n; i++) {
+                sigmaX1 += xData[0][i];
+                sigmaX2 += xData[1][i];
+                sigmaX12 += xData[0][i] * xData[1][i];
+                sigmaXsqr1 += xData[0][i] * xData[0][i];
+                sigmaXsqr2 += xData[1][i] * xData[1][i];
+            }
+            d = ((n * sigmaXsqr1 * sigmaXsqr2) + (sigmaX1 * sigmaX12 * sigmaX2) + (sigmaX2 * sigmaX1 * sigmaX12)
+                    - (sigmaX2 * sigmaXsqr1 * sigmaX2) - (n * sigmaX12 * sigmaX12) - (sigmaXsqr2 * sigmaX1 * sigmaX1));
         }
-        d = ((n * sigmaXsqr1 * sigmaXsqr2) + (sigmaX1 * sigmaX12 * sigmaX2) + (sigmaX2 * sigmaX1 * sigmaX12)
-                - (sigmaX2 * sigmaXsqr1 * sigmaX2) - (n * sigmaX12 * sigmaX12) - (sigmaXsqr2 * sigmaX1 * sigmaX1));
     }
 
     private void betas() {
-        double sigmaY = 0.0;
-        double sigmaX1Y = 0.0;
-        double sigmaX2Y = 0.0;
-        determinant();
-        for (int i = 0; i < n; i++) {
-            sigmaY += yData[i];
-            sigmaX1Y += xData[0][i] * yData[i];
-            sigmaX2Y += xData[1][i] * yData[i];
+        if(!trained) {
+            double sigmaY = 0.0;
+            double sigmaX1Y = 0.0;
+            double sigmaX2Y = 0.0;
+            for (int i = 0; i < n; i++) {
+                sigmaY += yData[i];
+                sigmaX1Y += xData[0][i] * yData[i];
+                sigmaX2Y += xData[1][i] * yData[i];
+            }
+            beta_0 = (((sigmaY * sigmaXsqr1 * sigmaXsqr2) + (sigmaX1 * sigmaX12 * sigmaX2Y)
+                    + (sigmaX2 * sigmaX1Y * sigmaX12) - (sigmaX2Y * sigmaXsqr1 * sigmaX2) - (sigmaX12 * sigmaX12 * sigmaY)
+                    - (sigmaXsqr2 * sigmaX1Y * sigmaX1)) / d);
+            beta_1 = (((sigmaX1Y * sigmaXsqr1 * sigmaXsqr2) + (sigmaX1 * sigmaX12 * sigmaX2Y)
+                    + (sigmaX2 * sigmaX1Y * sigmaX12) - (sigmaX2Y * sigmaXsqr1 * sigmaX2) - (sigmaX12 * sigmaX12 * sigmaX1Y)
+                    - (sigmaXsqr2 * sigmaX1Y * sigmaX1)) / d);
+            beta_2 = (((sigmaX2Y * sigmaXsqr1 * sigmaXsqr2) + (sigmaX1 * sigmaX12 * sigmaX2Y)
+                    + (sigmaX2 * sigmaX1Y * sigmaX12) - (sigmaX2Y * sigmaXsqr1 * sigmaX2) - (sigmaX12 * sigmaX12 * sigmaX2Y)
+                    - (sigmaXsqr2 * sigmaX1Y * sigmaX1)) / d);
         }
-        beta_0 = (((sigmaY * sigmaXsqr1 * sigmaXsqr2) + (sigmaX1 * sigmaX12 * sigmaX2Y)
-                + (sigmaX2 * sigmaX1Y * sigmaX12) - (sigmaX2Y * sigmaXsqr1 * sigmaX2) - (sigmaX12 * sigmaX12 * sigmaY)
-                - (sigmaXsqr2 * sigmaX1Y * sigmaX1)) / d);
-        beta_1 = (((sigmaX1Y * sigmaXsqr1 * sigmaXsqr2) + (sigmaX1 * sigmaX12 * sigmaX2Y)
-                + (sigmaX2 * sigmaX1Y * sigmaX12) - (sigmaX2Y * sigmaXsqr1 * sigmaX2) - (sigmaX12 * sigmaX12 * sigmaX1Y)
-                - (sigmaXsqr2 * sigmaX1Y * sigmaX1)) / d);
-        beta_2 = (((sigmaX2Y * sigmaXsqr1 * sigmaXsqr2) + (sigmaX1 * sigmaX12 * sigmaX2Y)
-                + (sigmaX2 * sigmaX1Y * sigmaX12) - (sigmaX2Y * sigmaXsqr1 * sigmaX2) - (sigmaX12 * sigmaX12 * sigmaX2Y)
-                - (sigmaXsqr2 * sigmaX1Y * sigmaX1)) / d);
     }
 
     public String equationMLR() {
-        betas();
-        StringBuilder s = new StringBuilder();
-        s.append(String.format("y = %.2f + %.2fx_1 + %.2fx_2", beta_0, beta_1, beta_2));
-        return s.toString();
+        if(trained) {
+            StringBuilder s = new StringBuilder();
+            s.append(String.format("y = %.2f + %.2fx_1 + %.2fx_2", beta_0, beta_1, beta_2));
+            return s.toString();
+        }
+        return("MLR is not trained yet!");
     }
 
     public double predict(double x_1, double x_2) {
-        return (beta_0 + (beta_1 * x_1) + (beta_2 * x_2));
+        if(trained) {
+            return (beta_0 + (beta_1 * x_1) + (beta_2 * x_2));
+        }
+        return(-1);
     }
 
-    public static void main(String[] args) {
-        MultipleLinearRegression mlr = new MultipleLinearRegression("startups.csv");
-        System.out.println(mlr.equationMLR());
+    public void train() {
+        if(!trained) {
+            determinant();
+            betas();
+            trained = true;
+        }
+    }
+
+    public boolean isTrained() {
+        return trained;
     }
 }
